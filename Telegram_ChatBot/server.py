fcd1e0bf2892f78a00da149cf5ad93d594ebf39b
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2021-02-28 22:44:14 +0530

    Update server.py

--- Telegram_ChatBot/server.py
+++ Telegram_ChatBot/server.py
@@ -1,20 +1,19 @@
 from bot import telegram_bot
-import Scrap
+import Scrap 
 
 bot = telegram_bot("config.cfg")
 
 def make_reply(msg,f_name):
-    reply= None
+    reply = None
     if msg == "":
         reply =  "Hello {}, Welcome to My Telegram Bot. Enter -- /help -- for help".format(f_name)
     elif msg == "/help":
-        reply = "I can help you please enter the tradecompany's symbol ex: ebl for Everest Bank limited  \n "
+        reply = "Commands Available. \n 1.I can help you please enter the tradecompany's symbol ex: ebl for Everest Bank limited \n 2. /end --> End the connection \n "
     else:
         reply=Scrap.StockPrice(msg)
     return reply
 
 update_id = None
-fl = True
 pName = {""}
 while True:
     updates = bot.get_updates(offset=update_id)
@@ -29,10 +28,14 @@ while True:
                 message = None
                 f_name=None
             from_ = item["message"]["from"]["id"]
-            if(pName == "" or f_name not in pName):
-                reply = make_reply("",f_name)
-                bot.send_message(reply,from_)
-                pName.add(f_name)
+            if(message == '/end' and f_name in pName):
+                bot.send_message("See You Again! \n Ending Connection...",from_)
+                pName.remove(f_name)
             else:
-                reply = make_reply(message,f_name)
-                bot.send_message(reply,from_)
+                if(pName == "" or f_name not in pName):
+                    reply = make_reply("",f_name)
+                    bot.send_message(reply,from_)
+                    pName.add(f_name)
+                else:
+                    reply = make_reply(message,f_name)
+                    bot.send_message(reply,from_)
